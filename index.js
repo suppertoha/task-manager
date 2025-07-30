6756a66b7c4d260bdad51a374cf5dea391690fa6
Suppertoha <anton.sherban@infourok.biz>
2025-07-30 09:13:48 +0300

    модуль f2

--- index.js
+++ index.js
@@ -1,44 +1,95 @@
-let task = "Задача изучить JavaScript";
+const tasks = [
+  {
+    title: "Задача изучить JavaScript",
+    description: "Повысить квалификацию",
+    isCompleted: false,
+    createdDate: new Date(), // тут опечатка в названии переменной (в задании).
+    completedDate: {},
+  },
+	{
+    title: "Задача модуль React",
+    description: "Повысить квалификацию React разработчика",
+    isCompleted: false,
+    createdDate: new Date(),
+    completedDate: {},
+  },
+];
+const completedTask = [];
 let completedTaskCount = 0;
 
 // * 1 showTask
 const showTask = () => {
-  if (!task) {
-    console.log("Задача отсутсвует");
-    return;
+  if (tasks.length !== 0) {
+    let result = "";
+    for (let i = 0; i < tasks.length; i++) {
+      result += `${tasks[i].title} - ${tasks[i].description}; `;
+    }
+    return result;
   }
-
-  console.log(task);
 };
 
 showTask();
+console.log("function showTask", showTask());
 
 // * 2 setTask
 
-const setTask = (taskDescription) => {
-  if (task) {
-    console.log("Не могу добавить задачу, завершите или удалите предыдущую");
-    return;
+const setTask = (title, description) => {
+	const newTasks = {
+    title,
+    description,
+    isCompleted: false,
+    createdDate: new Date(),
+    completedDate: {},
   }
-
-  task = taskDescription;
+	return tasks.push(newTasks)
 };
 
-setTask("Изучил модуль F1");
+setTask("Закончить модуль F2", "Перейти к другому модулю");
+console.log("function tasks", tasks);
 
 // * 3 completeTask
 
-const completeTask = () => {
-  completedTaskCount++;
-  task = "";
+const completeTask = (index) => {
+  for (let i = 0; i < tasks.length; i++) {
+    if (i === index) {
+      completedTask.push(tasks[index]);
+			tasks[index].isCompleted = true;
+			tasks[index].completedDate = new Date();
+      completedTaskCount++;
+    }
+  }
+
+  return completedTask;
 };
 
-completeTask()
+completeTask(2);
+console.log("function completeTask", completedTask);
 
 // * 4 deleteTask
 
-const deleteTask = () => {
-  task = "";
+const deleteTask = (index) => {
+	const newTasks = [];
+  for (let i = 0; i < tasks.length; i++) {
+    if (i !== index) {
+			newTasks.push(tasks[i])
+    }
+  }
+	tasks.length = 0;
+	for(let i = 0; i < newTasks.length; i++){
+		tasks.push(newTasks[i])
+	}
+  return tasks;
 };
 
-deleteTask();
+deleteTask(2);
+console.log("function deleteTask", deleteTask);
+
+// * 5 clearTasks
+
+const clearTasks = () => {
+  return (tasks.length = 0);
+};
+
+clearTasks();
+
+console.log("function clearTasks", tasks);
